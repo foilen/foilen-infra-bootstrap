f2d8cb290c076dc02ecb57350cc45df550063285
Simon Levesque <simon@simonlevesque.com>
2018-06-26 06:53:04 -0400

    Update copyright year

--- src/main/java/com/foilen/infra/bootstrap/InfraBootstrapApp.java
+++ src/main/java/com/foilen/infra/bootstrap/InfraBootstrapApp.java
@@ -1,7 +1,7 @@
 /*
     Foilen Infra Bootstrap
     https://github.com/foilen/foilen-infra-bootstrap
-    Copyright (c) 2017 Foilen (http://foilen.com)
+    Copyright (c) 2017-2018 Foilen (http://foilen.com)
 
     The MIT License
     http://opensource.org/licenses/MIT
--- src/main/java/com/foilen/infra/bootstrap/InfraBootstrapSpringConfig.java
+++ src/main/java/com/foilen/infra/bootstrap/InfraBootstrapSpringConfig.java
@@ -1,3 +1,12 @@
+/*
+    Foilen Infra Bootstrap
+    https://github.com/foilen/foilen-infra-bootstrap
+    Copyright (c) 2017-2018 Foilen (http://foilen.com)
+
+    The MIT License
+    http://opensource.org/licenses/MIT
+
+ */
 package com.foilen.infra.bootstrap;
 
 import org.springframework.context.annotation.Bean;
