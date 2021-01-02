8cb54d8550abce7a02c355e310a6c511fd4ffd21
Simon Levesque <simon@foilen.com>
2021-01-02 12:49:31 -0500

    Update copyright

--- src/main/java/com/foilen/infra/bootstrap/InfraBootstrapSpringConfig.java
+++ src/main/java/com/foilen/infra/bootstrap/InfraBootstrapSpringConfig.java
@@ -1,7 +1,7 @@
 /*
     Foilen Infra Bootstrap
     https://github.com/foilen/foilen-infra-bootstrap
-    Copyright (c) 2017-2019 Foilen (http://foilen.com)
+    Copyright (c) 2017-2021 Foilen (https://foilen.com)
 
     The MIT License
     http://opensource.org/licenses/MIT
