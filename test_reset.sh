ebc1bfbe9d27dcf7d92608e7eb3fcd6fb9de319f
Simon Levesque <simon@foilen.com>
2021-02-11 21:16:15 -0500

    fixup! Add test scripts

--- test_reset.sh
+++ test_reset.sh
@@ -6,7 +6,7 @@ cd $RUN_PATH
 docker stop infra_redirector_exit infra_web-vm-18 infra_ui infra_login infra_ui_db infra_login_db infra_bind9 infra_docker_manager infra_web-localhost_foilen-lab_com infra_redirector_entry infra_ui_db_manager
 docker rm infra_redirector_exit infra_web-vm-18 infra_ui infra_login infra_ui_db infra_login_db infra_bind9 infra_docker_manager infra_web-localhost_foilen-lab_com infra_redirector_entry infra_ui_db_manager
 
-rm -rf _log _log.txt
+rm -rf _logs _log.txt
 
 sudo deluser infra_bind9
 sudo deluser infra_login
@@ -26,4 +26,4 @@ sudo groupdel infra_web
 sudo groupdel infra_docker_manager
 sudo groupdel infra_url_redirection
 
-sudo rm -rf /home/infra_*
+sudo rm -rf /home/infra_* /var/infra-apps/ /var/infra-endpoints/
